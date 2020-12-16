7d73cc9e5a0356598dd7bc7cd849136c2b7c3e48
Jeferson Araujo <jaraujo@eaibrasil.com>
2020-12-16 15:29:37 -0300

    wip

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "reactstrap";
+
+function Home() {
+  return (
+    <div>
+      <Container>
+        <Row className="text-center">
+          <Col>
+            <h2 className="mt-5">Ambiente de produção</h2>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
